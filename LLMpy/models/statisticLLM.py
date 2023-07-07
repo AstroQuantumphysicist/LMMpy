024823355eaea064a5b2b49a3d742a5f71ec4de1
AstroQuantumphysicist <117011212+AstroQuantumphysicist@users.noreply.github.com>
2023-07-07 21:45:30 +0200

    Update statisticLLM.py

--- LLMpy/models/statisticLLM.py
+++ LLMpy/models/statisticLLM.py
@@ -6,28 +6,25 @@
 import random
 
 
-def create_keyword_dict(text, context_length):
+def create_keyword_dict(text):
     """
-    This funktion creates a dictionary of all keys (with context_length) and their next possible values.
+    This funktion creates a dictionary of all keys and their next possible values.
 
     Parameters
     ----------
     text : str
         The text to be analyzed.
-
-    context_length : int
-        The length of the context.
     """
     # split text into words and lowercase
     words = text.lower().split()
     # create dictionary
     keyword_dict = {}
     # loop through all words
-    for i in range(len(words) - context_length):
-        # create key
-        key = tuple(words[i:i + context_length])
+    for i in range(len(words) - 1):
+        # create keyword
+        keyword = words[i]
         # check next word
-        next_word = words[i + context_length]
+        next_word = words[i + 1]
         # check if key already exists
         if key in keyword_dict:
             # append next word
@@ -39,36 +36,31 @@ def create_keyword_dict(text, context_length):
     # return dictionary
     return keyword_dict
 
-def generate_text(start_words, keyword_dict, context_length):
+def generate_text(start_word, keyword_dict):
     """
     This funktion generates a text based on the keyword_dict.
 
     Parameters
     ----------
-    start_words : str
-        The start words for the text.
+    start_word : str
+        The start word for the text.
 
     keyword_dict : dict
         The dictionary of all keys (with context_length) and their next possible values.
-
-    context_length : int
-        The length of the context.
     """
-    # set current words
-    current_words = start_words.split()
+    # set current word
+    current_word = start_word
     # set generated text
-    generated_text = current_words.copy()
+    generated_text = current_word.copy()
 
     # loop through all words till end of text
-    while tuple(current_words[-context_length:]) != ("none",):
+    while current_word.lower() != "none":
         # choose next word
-        next_word = random.choice(keyword_dict.get(tuple(current_words[-context_length:]), ["None"]))
+        next_word = random.choice(keyword_dict[current_word.lower()]
         # append next word
         generated_text.append(next_word)
         # set current words
-        current_words.append(next_word)
-        # remove first word
-        current_words = current_words[1:]
+        current_words = next_word
 
     # return generated text
     return " ".join(generated_text)
